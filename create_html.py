c5f0947d427552065b54c864f1339cf7728c454f
Sahil Ahmed <sahil.a654@gmail.com>
2023-02-03 15:47:40 +0100

    Changed to a function call for all files

--- create_html.py
+++ create_html.py
@@ -1,10 +1,12 @@
 import pylightxl as xl
 
-# TODO: Make the fiel usable for all excel files.
+# TODO: Make the file usable for all excel files.
 
-if __name__ == "__main__":
-    fn_paper = "Special issues - call for papers.xlsx"
-    fn_paper_sheet = "Special issues"
+def generate_out(file_params):
+    fn_paper = file_params["file_name"]
+    fn_paper_sheet = file_params["sheet_name"]
+    table_name = file_params["out_name"]
+
     db = xl.readxl(fn_paper)
     start = db.ws(ws=fn_paper_sheet).address(address="A5")
     print(start)
@@ -46,18 +48,43 @@ if __name__ == "__main__":
 
 
     # Define the table data
-    db.add_nr(name="special_issues", ws=fn_paper_sheet, address=f"{table_start_cell}:{table_end_cell}")
+    db.add_nr(name=table_name, ws=fn_paper_sheet, address=f"{table_start_cell}:{table_end_cell}")
     # print(db.nr(name="special_issues"))
 
-    with open("out/special_issue.js", "w") as f:
+    # Find index of Link column
+    header = db.nr(name=table_name)[0]
+    print(header)
+    link_idx = header.index("Link")
+    print(link_idx)
+
+
+    with open(f"out/{table_name}.js", "w") as f:
         f.write("var dataSet = [\n")
-        for line in db.nr(name="special_issues")[1:]:
+        for line in db.nr(name=table_name)[1:]:
             f.write("[\n '")
-            f.write("', '".join(x.strip().replace("\n", " ") for x in line[:-3]))
+            f.write("', '".join(x.strip().replace("\n", " ") for x in line[:link_idx]))
             f.write("', '")
-            f.write(f'<a href="{line[-3]}">Link</a>')
+            f.write(f'<a href="{line[link_idx]}" target="_blank">Link</a>')
             f.write("', '")
-            f.write("', '".join(x.strip().replace("\n", " ") for x in line[-2:]))
+            f.write("', '".join(x.strip().replace("\n", " ") for x in line[link_idx+1:]))
             f.write("' ],\n")
         f.write("];")
         
+
+if __name__ == "__main__":
+    file_params = [{"file_name": "in/Special issues - call for papers.xlsx",
+                    "sheet_name": "Special issues",
+                    "out_name": "special_issues"},
+                    {"file_name": "in/Seminars webinars etc.xlsx",
+                    "sheet_name": "Seminars, WS, etc",
+                    "out_name": "seminar_webinar"},
+                    {"file_name": "in/Research conference calls.xlsx",
+                    "sheet_name": "Research conf",
+                    "out_name": "research_conf"},
+                    {"file_name": "in/Research calls.xlsx",
+                    "sheet_name": "Research calls",
+                    "out_name": "research_calls"},
+                ]
+
+    for file_param in file_params:
+        generate_out(file_param)
