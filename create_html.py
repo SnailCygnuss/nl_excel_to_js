abae5aeec90ea12b8dd32994ba6c227e127abd96
Sahil Ahmed <sahil.a654@gmail.com>
2023-02-14 15:36:23 +0100

    Removed unwated print statements

--- create_html.py
+++ create_html.py
@@ -8,11 +8,8 @@ def generate_out(file_params):
     fn_paper_sheet = file_params["sheet_name"]
     table_name = file_params["out_name"]
 
+    print(f"Processing {fn_paper}")
     db = xl.readxl(fn_paper)
-    start = db.ws(ws=fn_paper_sheet).address(address="A5")
-    print(start)
-    start = db.ws(ws=fn_paper_sheet).address(address="B2")
-    print(start)
     
     # Find the starting row of the table
     # iterates through column B to find the first cell with data
@@ -24,7 +21,7 @@ def generate_out(file_params):
         value = db.ws(ws=fn_paper_sheet).index(row=check_row, col=check_col)
     table_start_row = check_row
     table_start_cell = f"A{check_row}"
-    print(table_start_cell)
+    # print(table_start_cell)
 
     # Find end column of table
     check_row = table_start_row
@@ -45,19 +42,15 @@ def generate_out(file_params):
         value = db.ws(ws=fn_paper_sheet).index(row=check_row, col=check_col)
     table_end_row = check_row - 1
     table_end_cell = xl.pylightxl.utility_index2address(table_end_row, table_end_col)
-    print(table_end_cell)
+    # print(table_end_cell)
 
 
     # Define the table data
     db.add_nr(name=table_name, ws=fn_paper_sheet, address=f"{table_start_cell}:{table_end_cell}")
-    # print(db.nr(name="special_issues"))
 
-    # Find index of Link column
+    # Find index of Link column to make it hyperlink
     header = db.nr(name=table_name)[0]
-    print(header)
     link_idx = header.index("Link")
-    print(link_idx)
-
 
     with open(f"out/{table_name}.js", "w") as f:
         f.write("var dataSet = [\n")
@@ -79,16 +72,16 @@ def file_edit_date():
 
 
 if __name__ == "__main__":
-    file_params = [{"file_name": "in/Special issues - call for papers.xlsx",
+    file_params = [{"file_name": "Special issues - call for papers.xlsx",
                     "sheet_name": "Special issues",
                     "out_name": "special_issues"},
-                    {"file_name": "in/Seminars webinars etc.xlsx",
+                    {"file_name": "Seminars webinars etc.xlsx",
                     "sheet_name": "Seminars, WS, etc",
                     "out_name": "seminar_webinar"},
-                    {"file_name": "in/Research conference calls.xlsx",
+                    {"file_name": "Research conference calls.xlsx",
                     "sheet_name": "Research conf",
                     "out_name": "research_conf"},
-                    {"file_name": "in/Research calls.xlsx",
+                    {"file_name": "Research calls.xlsx",
                     "sheet_name": "Research calls",
                     "out_name": "research_calls"},
                 ]
